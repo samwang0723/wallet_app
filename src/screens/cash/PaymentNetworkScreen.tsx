321f3907b5a14a3250f0de9b89ffcf19528c1ef3
Sam Wang <sam.wang.0723@gmail.com>
2025-02-03 09:51:32 +0800

    fix: ignore error of explicitly any type

--- src/screens/cash/PaymentNetworkScreen.tsx
+++ src/screens/cash/PaymentNetworkScreen.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 import React from 'react';
 import { Text, View, ScrollView, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
