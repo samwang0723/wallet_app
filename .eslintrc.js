e7f67bfc0ddad144773fe9148d02a04ce21e0792
Sam Wang <sam.wang.0723@gmail.com>
2025-02-03 09:22:32 +0800

    feat: support environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,7 +49,7 @@ module.exports = {
     // TypeScript specific rules
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-non-null-assertion': 'warn',
 
